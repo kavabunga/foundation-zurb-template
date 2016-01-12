77377d5d292e1bedcc755a650501595140dce927
Semen Kats <kavabunga@gmail.com>
2016-01-13 02:03:58 +0300

    Added JS for highlighting selected nav item
    From the previous site version

---
+++ src/assets/js/navigation.js
@@ -0,0 +1,12 @@
+// Background change on selected menu items (Stacey)
+// var nav_items = document.getElementById('main-menu').getElementsByTagName('a');
+// var stripped_url = document.location.href.match(/([^\/]+)\/(#.+)?$/)[1];
+// for(var i = 0; i < nav_items.length; i++) {
+//   var slug = nav_items[i].href.match(/([^\/]+)\/$/);
+// 	var stripped_href = (slug) ? slug[1] : '';
+// 	if(stripped_url == stripped_href) {
+// 		nav_items[i].parentNode.className = 'selected';
+// 		nav_items[i].parentNode.insertBefore(document.createTextNode("\u203a "), nav_items[i]);
+// 		break;
+// 	}
+// }
